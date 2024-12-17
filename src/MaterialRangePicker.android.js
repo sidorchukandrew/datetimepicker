408eb99d2854e910d1eb12c124a283642bb92f87
sidorchukandrew <36050911+sidorchukandrew@users.noreply.github.com>
2024-12-17 15:30:35 -0500

    Implement imperative MaterialRangePicker
    Since this is a new module, I think we should only support the
    imperative API the same way we encourage developers to use the
    imperative API for the date and time pickers.

---
+++ src/MaterialRangePicker.android.js
@@ -0,0 +1,57 @@
+/**
+ * @format
+ * @flow strict-local
+ */
+
+import type {MaterialRangeProps, Range} from './types';
+import RNMaterialRangePickerAndroid from './specs/NativeModuleMaterialRangePicker';
+import {RANGE_SET_ACTION} from './constants';
+import {createDismissEvtParams, createRangeSetEvtParams} from './eventCreators';
+
+async function open({
+  onChange,
+  value = {},
+  maximumDate,
+  minimumDate,
+  onError,
+  ...props
+}: MaterialRangeProps = {}) {
+  try {
+    const result = await RNMaterialRangePickerAndroid.open({
+      startTimestamp: value.start?.getTime(),
+      endTimestamp: value.end?.getTime(),
+      maximumDate: maximumDate?.getTime(),
+      minimumDate: minimumDate?.getTime(),
+      ...props,
+    });
+
+    const {action, startTimestamp, endTimestamp, utcOffset} = result;
+
+    switch (action) {
+      case RANGE_SET_ACTION: {
+        const range: Range = {
+          start: new Date(startTimestamp),
+          end: new Date(endTimestamp),
+        };
+
+        const event = createRangeSetEvtParams(range, utcOffset);
+        onChange?.(event, range);
+        break;
+      }
+      case DISMISS_ACTION:
+      default: {
+        const [event] = createDismissEvtParams(originalValue, utcOffset);
+        onChange?.(event);
+        break;
+      }
+    }
+  } catch (error) {
+    onError?.(error);
+  }
+}
+
+function dismiss(): Promise<boolean> {
+  return RNMaterialRangePickerAndroid.dismiss();
+}
+
+export const MaterialRangePicker = {open, dismiss};
